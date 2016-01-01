4ddf282420ba45456a6c4d131dd7e0efa95dd86b
MintPaw <jerusanders@gmail.com>
2015-12-31 19:01:19 -0700

    Added entery point

--- src/Main.hx
+++ src/Main.hx
@@ -1,19 +1,31 @@
 package;
 
-
 import openfl.display.Sprite;
+import openfl.events.Event;
+import flixel.FlxGame;
 
+class Main extends Sprite
+{
+	
+	public function new()
+	{
+		super();
 
-class Main extends Sprite {
-	
-	
-	public function new () {
-		
-		super ();
-		
-		
-		
+		addEventListener(Event.ENTER_FRAME, init);
 	}
-	
-	
+
+	private function init(e:Event):Void
+	{
+		removeEventListener(Event.ENTER_FRAME, init);
+
+		var flixel:FlxGame = new FlxGame( 
+				stage.stageWidth,
+				stage.stageHeight,
+				MainState,
+				1,
+				60,
+				60,
+				true);
+		addChild(flixel);
+	}
 }
