a40fdd40b9000c6e0f848acc857fb4735a714f67
MintPaw <jerusanders@gmail.com>
2016-01-01 09:59:20 -0700

    Changed background color correctly, added camera scrolling, and resized map

--- src/MainState.hx
+++ src/MainState.hx
@@ -16,9 +16,10 @@ class MainState extends FlxState
 	{
 		super.create();
 		haxe.Log.trace = myTrace;
+		FlxG.camera.bgColor = 0xFFFF00FF;
 
-		MapGen.mapWidth = 50;
-		MapGen.mapHeight = 50;
+		MapGen.mapWidth = 30;
+		MapGen.mapHeight = 30;
 		MapGen.minRooms = 3;
 		MapGen.maxRooms = 6;
 		MapGen.minRoomSize = 2;
@@ -38,7 +39,17 @@ class MainState extends FlxState
 				1);
 		add(tilemap);
 	}
-	
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+
+		var scrollSpeed:Int = 20;
+		if (FlxG.keys.pressed.UP) FlxG.camera.scroll.y -= scrollSpeed;
+		if (FlxG.keys.pressed.DOWN) FlxG.camera.scroll.y += scrollSpeed;
+		if (FlxG.keys.pressed.LEFT) FlxG.camera.scroll.x -= scrollSpeed;
+		if (FlxG.keys.pressed.RIGHT) FlxG.camera.scroll.x += scrollSpeed;
+	}
 
 	private function myTrace(d:Dynamic, ?i:Null<haxe.PosInfos>):Void
 	{
